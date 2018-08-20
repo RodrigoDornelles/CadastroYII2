854885305cd155aa5d928d4ca434459c0c4f3d3b
RodrigoDornelles <razorgmenturion@gmail.com>
2018-08-20 18:15:57 -0300

    Add files via upload

---
+++ views/pessoa/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\data\Pagination;
+use yii\grid\GridView;
+use yii\widgets\LinkPager;
+use yii\widgets\BaseListView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\PessoaSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Teste de Conhecimento';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="pessoa-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Pessoa', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        //'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'nome',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
---
+++ views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Teste de Conhecimento';
+?>
+<div class="site-index">
+
+      
+
+</div>
